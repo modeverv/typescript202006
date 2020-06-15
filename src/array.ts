5d5f507b532db270dae690cf869e7f6544a71876
modeverv <modeverv@gmail.com>
2020-06-15 21:42:49 +0900

    2020-06-15 21:42:49

---
+++ src/array.ts
@@ -0,0 +1,13 @@
+export { };
+    
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings: string[] = ['A', 'bbb', 'C']
+
+let numnum :number[][]= [
+    [1, 2],
+    [3, 4],
+];
+
+let hh: (string | number | boolean)[] = [1, false, 'japan'];
