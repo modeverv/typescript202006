cd58a834291c872f966e3b92e8d19fcc24205c53
modeverv <modeverv@gmail.com>
2020-06-16 22:40:34 +0900

    2020-06-16 22:40:34

---
+++ src/interfaces.ts
@@ -0,0 +1,20 @@
+export {};
+
+type ObjectType = {
+  name: string;
+  age: number;
+};
+let object: ObjectType = {
+  name: 'hoe',
+  age: 32,
+};
+
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+let o2: ObjectInterface = {
+  name: 'afe',
+  age: 32,
+};
