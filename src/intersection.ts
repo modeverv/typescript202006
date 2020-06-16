a6302469c2d2075cff14ec033f557c7c4b03f515
modeverv <modeverv@gmail.com>
2020-06-16 23:11:39 +0900

    2020-06-16 23:11:39

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,1 +1,25 @@
-export {};
+
+export { };
+    
+type Pitcher1 = {
+    throwSpeed: number;
+}
+type Batter1 = {
+    battingAverage: number;
+}   
+const Sasaki: Pitcher1 = {
+    throwSpeed: 154
+};
+ 
+const Ochiai: Batter1 = {
+    battingAverage: 0.367
+};
+
+// インターセクション型
+type TwoWayPlayer = Pitcher1 & Batter1;
+
+const Ohtani: TwoWayPlayer = {
+    throwSpeed: 165,
+    battingAverage: 0.286
+}
+
