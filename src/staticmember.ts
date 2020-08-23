b7ce10e6ba95454d73906ef37924e1482327c4f7
modeverv <modeverv@gmail.com>
2020-08-23 21:00:57 +0900

    2020-08-23 21:00:57

--- src/staticmember.ts
+++ src/staticmember.ts
@@ -1,1 +1,18 @@
 export {};
+
+class Me {
+    static isProgramaer: boolean = true;
+    static firstName: string = 'Atsushi';
+    static lastName: string = 'Ishida'
+    static work() {
+        return `Hey Hey ${this.firstName}`;
+    }
+}
+
+let me = new Me();
+//console.log(me.isProgramaer);
+console.log(Me.isProgramaer);
+console.log(Me.work());
+
+
+
