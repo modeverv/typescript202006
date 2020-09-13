11fd2de7a7873329316710fc1f70c476350852df
modeverv <modeverv@gmail.com>
2020-09-13 16:29:36 +0900

    2020-09-13 16:29:35

--- src/pick-omit.ts
+++ src/pick-omit.ts
@@ -1,1 +1,11 @@
 export {};
+
+
+type DetailedProfile = {
+    name: string;
+    height:number;
+    weight: number;
+}
+type SimpleProfile = Pick<DetailedProfile, 'name' | 'height'>
+type Simeple2 = Omit<DetailedProfile, 'name' | 'height'>
+type MyOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
